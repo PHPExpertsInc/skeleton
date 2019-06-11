7979303b3edccf5ab4ee25145cfdda8ac4ce2699
Theodore R. Smith <theodore@phpexperts.pro>
2019-06-11 09:06:32 -0500

    Fixed the PHP-CS-Fixer issues in this skeleton.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,13 @@
-<?php
+<?php declare(strict_types=1);
 
+/**
+ * This file is part of Skeleton, a PHP Experts, Inc., Project.
+ *
+ * Copyright © 2019 PHP Experts, Inc.
+ * Author: Theodore R. Smith <theodore@phpexperts.pro>
+ *   GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *   https://www.phpexperts.pro/
+ *   https://github.com/PHPExpertsInc/Skeleton
+ *
+ * This file is licensed under the MIT License.
+ */
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,9 +5,9 @@
  *
  * Copyright © 2019 PHP Experts, Inc.
  * Author: Theodore R. Smith <theodore@phpexperts.pro>
- *  GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
- *  https://www.phpexperts.pro/
- *  https://github.com/phpexpertsinc/SimpleDTO
+ *   GPG Fingerprint: 4BF8 2613 1C34 87AC D28F  2AD8 EB24 A91D D612 5690
+ *   https://www.phpexperts.pro/
+ *   https://github.com/PHPExpertsInc/Skeleton
  *
  * This file is licensed under the MIT License.
  */
