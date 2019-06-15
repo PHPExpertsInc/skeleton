aba9697fd6c2ab9733f1e5e5cd8913cfff8fbd2a
Theodore R. Smith <theodore@phpexperts.pro>
2019-06-15 05:05:43 -0500

    Added PHPUnit isDebugOn() detection.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,4 +18,14 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * Checks if phpunit was togged in debug mode o rnot.
+     * See https://stackoverflow.com/a/12612733/430062.
+     *
+     * @return bool
+     */
+    public static function isDebugOn(): bool
+    {
+        return in_array('--debug', $_SERVER['argv'], true);
+    }
 }
