b387870d657cda1002cd701a76b6f9bc50a1c8c8
Theodore R. Smith <theodore@phpexperts.pro>
2021-12-25 13:02:41 -0600

    Upgraded to the latest php-cs-fixer.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,16 +12,19 @@ Author: Theodore R. Smith <theodore@phpexperts.pro>
 This file is licensed under the MIT License.
 HEADER;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony'       => true,
         'elseif'         => false,
         'yoda_style'     => false,
         'list_syntax'    => ['syntax'  => 'short'],
         'concat_space'   => ['spacing' => 'one'],
-        'binary_operator_spaces' => array(
-            'align_double_arrow' => true,
-        ),
+        'binary_operator_spaces' => [
+            'operators' => [
+                '='  => 'align',
+                '=>' => 'align',
+            ],
+        ],
         'phpdoc_no_alias_tag'          => false,
         'declare_strict_types'         => true,
         'no_superfluous_elseif'        => true,
